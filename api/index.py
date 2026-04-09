ce2c4e617afc4770d56c5d634e477c2034bda807
andydyan20 <dangtothedung@gmail.com>
2026-04-09 13:07:14 +0700

    vercel depoly

--- api/index.py
+++ api/index.py
@@ -6,8 +6,11 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parents[1]
 sys.path.insert(0, str(BASE_DIR))
 
+app = None
+
 try:
-    from apps.api.main import app
+    from apps.api.main import app as _app
+    app = _app
 except Exception as e:
     import traceback
     print("FATAL ERROR DURING IMPORT:")
